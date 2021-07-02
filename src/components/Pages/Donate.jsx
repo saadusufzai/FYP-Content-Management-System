1a07e9cc793d091afbb20259962e66431aea4259
Saad Aslam <saadusufzai@gmail.com>
2021-07-03 04:53:45 +0500

    Donate page and cart added

--- src/components/Pages/Donate.jsx
+++ src/components/Pages/Donate.jsx
@@ -1,14 +1,31 @@
-import { Typography } from '@material-ui/core'
-import React from 'react'
-import ContactForm from '../ContactForm/ContactForm'
+import { Typography } from "@material-ui/core";
+import React from "react";
+import ContactForm from "../ContactForm/ContactForm";
+import styles from "./Donate.module.css";
+import Grid from "@material-ui/core/Grid";
+
+import Product from "../Product/Product";
 
 const Donate = () => {
-    return (
-        <div>
-            <Typography align="center" varient="h3">This Page is Under Construction. Come Back Soon</Typography>
-            <ContactForm showText={true}/>
-        </div>
-    )
-}
+  const data = [1];
+  return (
+    <div>
+     <div className={styles.upper}>
+     <Grid container spacing={3}>
 
-export default Donate
+        <Grid item xs={12} sm={6} md={8} className={styles.left}>
+            <Typography variant="h2">Plant a Billion Trees</Typography>
+            <Typography variant="body1">Together we can plant roots for tomorrow and donate today. Your donation can turn the tide of deforestation and help us protect and expand forests around the globe before they shrink even further. You’ll do more than put a seed in the ground—you’ll make sure these habitats can survive for future generations.</Typography>
+        </Grid>
+        <Grid xs={12} sm={6} md={4} className={styles.right}>
+            <Product />
+        </Grid>
+        </Grid>
+     </div>
+
+      <ContactForm showText={true} />
+    </div>
+  );
+};
+
+export default Donate;
---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import styles from './Product.module.css';
+import trees from './tree.json'
+import img from '../../images/donate/tree2.jpg'
+
+const Product = () => {
+    return (
+        <div className={styles.featured}>
+    
+    {trees.map(tree => {
+    return (
+      <div key={tree.id} className={styles.card}>
+          <img src={img} alt={`Preview of ${tree.title}`} />
+        <h3>{ tree.title }</h3>
+        <p>{ tree.description }</p>
+        <p>${ tree.price }</p>
+        <p>
+          <button
+       className="snipcart-add-item btn btn-outline-danger btn-sm"
+       data-item-name={tree.title}
+       data-item-price={tree.price}
+       data-item-description={tree.description}
+       data-item-min-quantity="0"
+       data-item-max-quantity={tree.Qte}
+       data-item-id={tree.id}
+       data-item-url="/"
+          >Add to Cart</button>
+        </p>
+      </div>
+    );
+  })}
+      
+      
+    </div>
+    )
+}
+
+export default Product
